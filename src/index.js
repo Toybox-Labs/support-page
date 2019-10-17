7727389342a072edea880479c2b9123c03112c27
ToyboxLabs <25827093+ToyboxLabs@users.noreply.github.com>
2019-10-17 12:43:39 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import WebFont from 'webfontloader';
-
+window.location = "https://maketoys.zendesk.com/hc/en-us"
 WebFont.load({
   google: {
     families: ['Poppins:700,300,600', 'sans-serif']
